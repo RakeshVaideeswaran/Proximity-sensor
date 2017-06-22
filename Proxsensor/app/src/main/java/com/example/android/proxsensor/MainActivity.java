f41d101c7cd7fc3f6104f1cca10b1fd1c91c7124
Rakesh Vaideeswaran <raku18998@gmail.com>
2017-06-22 23:49:34 +0530

    Proximity sensor HACKERMODE

---
+++ Proxsensor/app/src/main/java/com/example/android/proxsensor/MainActivity.java
@@ -0,0 +1,138 @@
+package com.example.android.proxsensor;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import static android.os.Build.VERSION_CODES.M;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
+    TextView T;
+    SensorManager sm;
+    Sensor ps;
+    Ringtone dr = null;
+    RingtoneManager rm;
+    int i;
+    boolean RUN=false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        T = (TextView) findViewById(R.id.tv);
+        Uri tone = rm.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        dr = rm.getRingtone(this, tone);
+        sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        ps = sm.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event)
+    {
+
+        float d = event.values[0];
+
+        if(d==0)
+        {
+            new Task().execute();
+        }
+
+        else
+        {
+            dr.stop();
+            T.setVisibility(View.GONE);
+            RUN=true;
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        dr.stop();
+        sm.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        sm.registerListener(this,ps,SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+
+  class Task extends AsyncTask<Void,Void,Void>{
+
+    @Override
+    protected void onPreExecute() {
+
+        i=10;
+        T.setVisibility(View.VISIBLE);
+        T.setText(String.valueOf(i));
+        RUN=false;
+
+    }
+
+    @Override
+    protected void onCancelled() {
+        RUN=true;
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
+
+        while (i>=0 && !RUN)
+        {
+            publishProgress();
+
+            try
+            {
+            Thread.sleep(1000);
+            }
+            catch (InterruptedException e)
+            {
+            e.printStackTrace();
+            }
+
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+
+        T.setText(String.valueOf(i));
+
+        if (i==0)
+        {
+            dr.play();
+            T.setVisibility(View.GONE);
+            cancel(true);
+        }
+
+        i--;
+    }
+
+  }
+
+}
